c76d2222ebe5d7ebed7261c565d80c437e8496bc
dmac <DaraMac@users.noreply.github.com>
2021-12-31 21:59:15 +0000

    Fix nan values

--- ranker/ranker.py
+++ ranker/ranker.py
@@ -1,4 +1,6 @@
 import pandas as pd
 albums = pd.read_csv("../Albums.csv")
 
-
+c = albums[albums["Date Listened"].str.contains("2021")]
+# b = albums[["2021" in str(a) for a in albums["Date Listened"]]]
+# .sort_values(by="Tier")
