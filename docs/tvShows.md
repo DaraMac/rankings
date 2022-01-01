17453933775014892f1ed0698d97d446a7551ea7
dmac <DaraMac@users.noreply.github.com>
2022-01-01 01:45:59 +0000

    Rank tv shows

---
+++ docs/tvShows.md
@@ -0,0 +1,7 @@
+0.               Twin Peaks Season  1,  1
+1.               Twin Peaks Season  3,  1
+2.               Twin Peaks Season  2,  2
+3. I Think You Should Leave Season  1,  2
+4.               Invincible Season  1,  3
+5.  Serial Experiments Lain Season  1,  3
+6.               Squid Game Season  1,  3
--- ranker/ranker.py
+++ ranker/ranker.py
@@ -13,6 +13,9 @@ def albumer(n):
 def artister(n):
     return str(n) + ","
 
+def seasoner(n):
+    return "Season " + str(n) + ","
+
 # print(albums_ranked.to_string(index=False, header=False, formatters=[str, albumer, artister, str]))
 
 books = pd.read_csv("../Books.csv")
@@ -23,8 +26,13 @@ books_ranked.insert(0, "Rank Order", [str(i)+'.' for i in range(len(books_ranked
 # print(books_ranked.to_string(index=False, header=False, formatters=[str, albumer, artister, str]))
 
 movies = pd.read_csv("../Movies.csv")
-
 movies_ranked = movies[movies["Date Watched"].str.contains("2021")].sort_values(by="Tier")
 movies_ranked = movies_ranked[["Movie", "Tier"]]
 movies_ranked.insert(0, "Rank Order", [str(i)+'.' for i in range(len(movies_ranked))])
-print(movies_ranked.to_string(index=False, header=False, formatters=[str, artister, str]))
+# print(movies_ranked.to_string(index=False, header=False, formatters=[str, artister, str]))
+
+tv = pd.read_csv("../TvShows.csv")
+tv_ranked = tv[tv["Date Started"].str.contains("2021")].sort_values(by="Tier")
+tv_ranked = tv_ranked[["Show", "Season", "Tier"]]
+tv_ranked.insert(0, "Rank Order", [str(i)+'.' for i in range(len(tv_ranked))])
+print(tv_ranked.to_string(index=False, header=False, formatters=[str, str, seasoner, str]))
